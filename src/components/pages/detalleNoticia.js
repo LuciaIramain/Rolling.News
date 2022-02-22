33f8ad96d8041641a6e9628add004e1aba0e97b4
Lucia Iramain <luciairamain5@gmail.com>
2022-02-22 19:20:12 -0300

    Se corrigio la fecha en DetalleNoticia

--- src/components/pages/detalleNoticia.js
+++ src/components/pages/detalleNoticia.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import NavegacionAdm from "../common/NavegacionAdm";
 import Navegacion from "../common/Navegacion";
+import moment from 'moment';
 import "../css/estiloGeneral.css";
 
 const DetalleNoticia = (props) => {
@@ -33,9 +34,10 @@ const DetalleNoticia = (props) => {
       const res = await fetch(URL + "/" + id);
       if (res.status === 200) {
         const consulta = await res.json();
-        const fechaTrans = new Date(consulta.fecha);
-        const fechaTra = `${fechaTrans?.getDate()}-${fechaTrans?.getMonth()}-${fechaTrans?.getFullYear()}`;
-        setNoticia({...consulta, fechaTra});
+        const fechaTransf = new Date(consulta.fecha);
+        const fechaTran = new Date(fechaTransf.getTime() + fechaTransf.getTimezoneOffset() * 60000);
+        const fechaT = moment(fechaTran).format("DD-MM-YYYY");
+        setNoticia({...consulta, fechaT});
       }
     } catch (error) {
       console.log(error);
@@ -49,7 +51,7 @@ const DetalleNoticia = (props) => {
         <h2 className="my-5 colorTexto">{noticia.categoria}</h2> 
         <h1 className="my-4 colorTexto">{noticia.tituloNoticia}</h1>
         <p className="my-3 lead textoNoticia">{noticia.descripcionBreve}</p> 
-        <p className="my-2">{noticia.fechaTra}</p>
+        <p className="my-2">{noticia.fechaT}</p>
         <p className="my-2">{noticia.autor}</p> 
         <img src={noticia.imagen} alt="imagen noticia" className="my-3 imgNoticia"/>
         <p className="my-5 textoNoticia lh-1">{noticia.descripcionDetallada}</p> 
